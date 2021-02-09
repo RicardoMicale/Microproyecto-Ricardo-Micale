a1a8b393d70d7e90f6f0c26d7879cc3ba7277fdb
Ricardo Micale <ricardomicale@gmail.com>
2021-02-09 16:18:13 -0400

    listo about me

---
+++ main.js
@@ -0,0 +1,16 @@
+// const prev = document.getElementById(prev);
+// const next = document.getElementById(next);
+
+// let numeroImagen = 0;
+
+// next.addEventListener('click', () => {
+//     if(numeroImagen < 3) {
+//         if(numeroImagen === 0) {
+//             document.getElementById(imagen).src = './imagenes/avila.png';
+//         } else if(numeroImagen === 1) {
+//             document.getElementById(imagen).src = './imagenes/'
+//         }
+//     } else {
+//         numeroImagen = 0;
+//     }
+// })
